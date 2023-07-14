d7fc873e45d8d5432b9fd9b314487dfa0a93947d
JatoMixo <jatomixo@outlook.es>
2023-07-14 16:56:12 +0200

    Made simple design on excalidraw as a reference, started building the UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
+<script lang="ts">
+
+</script>
+
+<style lang="scss">
+
+</style>
+
 <h1>Testing...</h1>
 <p>Just... Stay here for a little bit more, I swear this is gonna look good at some point</p>
+
+<div id="calendar-grid">
+    <div class="day-element">
+        <p>Just testing</p>
+    </div>
+    <div class="day-element">
+        <p>Just testing</p>
+    </div>
+    <div class="day-element">
+        <p>Just testing</p>
+    </div>
+    <div class="day-element">
+        <p>Just testing</p>
+    </div>
+    <div class="day-element">
+        <p>Just testing</p>
+    </div>
+    <div class="day-element">
+        <p>Just testing</p>
+    </div>
+    <div class="day-element">
+        <p>Just testing</p>
+    </div>
+    <div class="day-element">
+        <p>Just testing</p>
+    </div>
+</div>
