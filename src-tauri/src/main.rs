5b7b45e9e7f99b0af7458f53de4db60b6c8bc8be
JatoMixo <jatomixo@outlook.es>
2023-07-15 13:55:21 +0200

    Fixed compiler errors on getting the user's folder

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,6 +6,9 @@ mod json;
 use json::json_formatter::{json_from_string, json_to_string};
 use json::{file_reader, file_writer, error};
 
+mod user_folder;
+use user_folder::get_user_folder;
+
 fn main() {
   tauri::Builder::default()
     .run(tauri::generate_context!())
--- src-tauri/src/user_folder.rs
+++ src-tauri/src/user_folder.rs
@@ -1,8 +1,8 @@
 use dirs::home_dir;
 
-pub fn get_user_folder() -> Result<&str, &str> {
+pub fn get_user_folder() -> Result<String, String> {
     match home_dir() {
-        Some(home_directory) => Ok(home_directory.to_string()),
-        None => Err("Unable to get home directory")
+        Some(home_directory) => Ok(home_directory.into_os_string().to_str().unwrap().to_string()),
+        None => Err("Unable to get home directory".to_string()),
     }
 }
