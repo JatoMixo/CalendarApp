e6ebc4138ba574c1e16d0c10ee95976e7e6e5682
JatoMixo <jatomixo@outlook.es>
2023-07-18 16:56:43 +0200

    Refactored Read JSON Cache, fixed small bug when pushing project to the cache

--- src-tauri/src/cache.rs
+++ src-tauri/src/cache.rs
@@ -11,19 +11,13 @@ pub fn read_json_cache() -> Value {
 
     let cache_path = &(get_user_folder().unwrap() + CACHE_NAME);
 
-    let mut json_data: Value = Value::Null;
-
     match json_from_file(&cache_path) {
-        Ok(value) => {
-            json_data = value;
-        },
-        Err(_error) => {
+        Ok(value) => value,
+        Err(_) => {
             create_cache().unwrap();
-            return read_json_cache();
-        },
+            read_json_cache()
+        }
     }
-
-    json_data
 }
 
 pub fn get_projects_from_cache() -> Vec<Project> {
@@ -31,7 +25,7 @@ pub fn get_projects_from_cache() -> Vec<Project> {
     let mut projects: Vec<Project> = Vec::new();
 
     for project_value in projects_as_values {
-        let project: Project = serde_json::from_value(project_value).unwrap();
+        let project: Project = Project::from_json(project_value).unwrap();
 
         projects.push(project);
     }
@@ -39,8 +33,23 @@ pub fn get_projects_from_cache() -> Vec<Project> {
     projects
 }
 
-pub fn push_project_to_cache() -> Result<(), FileError> {
-    Ok(())
+pub fn push_project_to_cache(project: Project) -> Result<(), FileError> {
+
+    let mut projects: Vec<Project> = get_projects_from_cache();
+    projects.push(project);
+
+    let json_cache = json!({
+        "projects": projects,
+    });
+
+    let mut cache_path: String = String::new();
+
+    match get_user_folder() {
+        Ok(user_folder) => cache_path = user_folder + CACHE_NAME,
+        Err(_) => return Err(FileError::CreateError),
+    }
+
+    write_json_to_file(&cache_path, json_cache)
 }
 
 pub fn remove_project_from_cache() -> Result<(), FileError> {
@@ -55,7 +64,15 @@ pub fn create_cache() -> Result<(), FileError> {
         ]
     });
 
-    let cache_path = &(get_user_folder().unwrap() + CACHE_NAME);
+    let user_folder: String;
 
-    Ok(())
+    match get_user_folder() {
+        Ok(new_user_folder) => user_folder = new_user_folder,
+        Err(_) => return Err(FileError::CreateError),
+    }
+
+    println!("{}", user_folder);
+    let cache_path = user_folder + CACHE_NAME;
+
+    write_json_to_file(&cache_path, cache_structure)
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,7 +10,7 @@ mod user_folder;
 use user_folder::{get_user_folder, CACHE_NAME};
 
 mod cache;
-use cache::{read_json_cache, get_projects_from_cache, create_cache};
+use cache::{read_json_cache, get_projects_from_cache, create_cache, push_project_to_cache};
 
 mod calendar;
 use calendar::project::Project;
@@ -19,6 +19,24 @@ use calendar::date::Date;
 use serde_json::json;
 
 fn main() {
+  let project = Project {
+    name: "Potato".to_string(),
+    description: "ASDASD".to_string(),
+    color: "#6f6f6f".to_string(),
+    start_date: Date {
+      day: 24,
+      month: 6,
+      year: 2023,
+    },
+    final_date: Date {
+      day: 30,
+      month: 7,
+      year: 2077,
+    },
+  };
+  println!("Created the cache: {:?}", create_cache());
+  println!("Pushed the new project: {:?}", push_project_to_cache(project));
+  println!("Projects from cache: {:?}", get_projects_from_cache());
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
